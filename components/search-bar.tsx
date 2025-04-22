f3f92f89bd2e97d2a2c9ef982f798ca504f96098
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-04-22 21:58:01 +0700

    add: search query and debounced

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -4,13 +4,15 @@ import { Image, TextInput, View } from 'react-native';
 interface SearchBarProps {
   onPress?: () => void;
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-export const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+export const SearchBar = ({ onPress, placeholder, value, onChangeText }: SearchBarProps) => {
   return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+    <View className="flex-row items-center bg-dark-100 rounded-l-full rounded-r-full px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
-      <TextInput onPress={onPress} placeholder={placeholder} value="" onChangeText={() => {}} placeholderTextColor="#a8b5db" className="ml-2 text-white" />
+      <TextInput onPress={onPress} placeholder={placeholder} value={value} onChangeText={onChangeText} placeholderTextColor="#a8b5db" className="ml-2 text-white" />
     </View>
   );
 };
